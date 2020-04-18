a13e5c874ce341fb60bfe4cfc84e054d95d6c165
Nishant Kishore <nish.kishore@gmail.com>
2020-04-18 17:10:31 -0400

    Update generate_plots.R
    updated to take the average of the runs

--- generate_plots.R
+++ generate_plots.R
@@ -6,6 +6,7 @@ source("./src/helper_functions.R")
 results_log <- read_csv("./results_log.csv")
 
 results_master <- load_run_results(results_log[1,"unique_id"])
+params <- results_log[1,] %>% as.list()
 
 #overall
 results_master %>%
@@ -15,17 +16,17 @@ results_master %>%
 
 #pre lockdown announcement summary + lag for inc period
 results_master %>%
-  filter(!is.na(Community) & DayInfected < (t_ld_a+5)) %>%
+  filter(!is.na(Community) & DayInfected < (params$t_ld_a+5)) %>%
   group_by(Community, Simulation) %>%
   summarise(n=n()) -> community_summary_prelockdown
 
 results_master %>%
-  filter(!is.na(Community) & DayInfected >=(t_ld_a+5) & DayInfected <= (t_ld_b+5)) %>%
+  filter(!is.na(Community) & DayInfected >=(params$t_ld_a+5) & DayInfected <= (params$t_ld_b+5)) %>%
   group_by(Community, Simulation) %>%
   summarise(n=n()) -> community_summary_postlockdown_a
 
 results_master %>%
-  filter(!is.na(Community) & Simulation & DayInfected >=(t_ld_b+5)) %>%
+  filter(!is.na(Community) & Simulation & DayInfected >=(params$t_ld_b+5)) %>%
   group_by(Community) %>%
   summarise(n=n()) -> community_summary_postlockdown_b
 
@@ -38,13 +39,16 @@ results_master %>%
   group_by(Community) %>%
   mutate(cumulative=cumsum(n)) -> community_day_summary
 
-ggplot(community_day_summary, aes(x=DayInfected,y=cumulative,group=factor(Community),color=factor(Community))) +
+community_day_summary %>%
+  ungroup() %>%
+  group_by(DayInfected, Community) %>%
+  #can change to median once we have more runs
+  summarise(avg_cumulative = mean(cumulative)) %>%
+  ggplot(aes(x=DayInfected,y=avg_cumulative,group=factor(Community),color=factor(Community))) +
   geom_line() + theme_classic() +
-  geom_vline(xintercept=t_ld_a) +
-  geom_vline(xintercept=t_ld_b) +
   labs(color="Community",
        x="Days",
        y="Cumulative cases",
-       caption = paste0("Starting community = ",start_comm,
+       caption = paste0("Starting community = ",params$start_comm,
                         "\n Total # cases = ", sum(community_day_summary$n),
                         "\n Vertical black lines indicate when lockdown was announced and began"))
