e3b495f23e9c4148a752b0cf9c3e5ad4fd7b7400
Nishant Kishore <nish.kishore@gmail.com>
2020-04-28 15:10:49 -0400

    Update nish_plotting.R

--- sandbox/nish_plotting.R
+++ sandbox/nish_plotting.R
@@ -2,7 +2,7 @@ source("./src/helper_functions.R")
 
 travel_probs <- read_rds("./testing/travel_probs.rds")
 results_log <- read_csv("./results_log.csv")
-ids <- results_log[17:nrow(results_log), "unique_id"]
+ids <- subset(results_log, comm_version == 2) %>% select(unique_id)
 
 lapply(1:nrow(ids), function(x) load_merge_vars(results_log, ids[x,], alpha_init, ld_b, beta_inc, alpha_inc, beta_dec, alpha_dec, Nruns)) %>%
   bind_rows() -> plot_data
@@ -26,7 +26,7 @@ plot_data %>%
             prob_epi_50 = round(sum(infections>=50)/10,2),
             av_start_time = round(mean(start - t_ld_a),1)) %>%
   ungroup() %>%
-  mutate(type = factor(type),
+  mutate(type = ifelse(type == "Urban", "City", "Non-city"),
          alpha_init = factor(alpha_init), 
          ld_b = factor(ld_b), 
          beta_inc = factor(beta_inc), 
@@ -35,31 +35,25 @@ plot_data %>%
          alpha_dec = factor(alpha_dec)) %>%
   left_join(travel_probs, by = "Community") -> out_data
 
-create_plot <- function(ld_b2, beta_inc2, beta_dec2){
-  out_data %>%
-    subset(ld_b == ld_b2 & beta_inc == beta_inc2 & beta_dec == beta_dec2) %>%
-    group_by(Community, type, alpha_inc, alpha_dec, prob_start_comm) %>%
-    summarize(prob = mean(prob_epi_1)) %>%
+create_plot <- function(data, alpha_inc2, ld_b2, cases){
+  data %>%
+    subset(ld_b == ld_b2 & alpha_inc == alpha_inc2) %>%
+    rename("prob" = paste0("prob_epi_",cases)) %>%
+    group_by(Community, type, beta_inc, beta_dec, prob_start_comm) %>%
+    summarize(prob = mean(prob)) %>%
     ungroup() %>%
-    ggplot(aes(x = log(prob_start_comm), y = prob, color = type)) + 
+    ggplot(aes(x = log(prob_start_comm), y = prob)) + 
     geom_point() + 
-    geom_smooth(method = lm) + 
-    facet_grid(alpha_dec~alpha_inc) + 
+    geom_smooth(method = "loess") + 
+    facet_grid(beta_dec~beta_inc) + 
     theme_bw() + 
-    labs(title = paste0("Delay: ", ld_b2, ", Beta(+): ",beta_inc2, ", Beta(-): ",beta_dec2), 
-         x = "Log probability of travel from start community", y = "Probability of epidemic (> 1 case)",
-         color = "Type")
+    labs(title = paste0("Delay: ", ld_b2, " days, Alpha(+): ",alpha_inc2,", Alpha(-): 0.5"), 
+         x = "Log probability of travel from start community", 
+         y = paste0("Probability of epidemic (> ",cases," cases)"))
 }
 
-out_data %>%
-  select(ld_b, beta_inc, beta_dec) %>%
-  distinct() -> data_types
 
-for(i in 1:nrow(data_types)){
-  in_data <- data_type[i,]
-  p <- create_plot(in_data$ld_b, in_data$beta_inc, in_data$beta_dec)
-  ggsave(paste0("./figs/scatter_plot_",in_data$ld_b,"-",in_data$beta_inc,"-",in_data$beta_dec,".png", dpi = 300))
-}
+ggarrange(create_plot(out_data, 5, 3, 10), create_plot(out_data, 5, 7, 10), ncol = 1)
 
 
 
