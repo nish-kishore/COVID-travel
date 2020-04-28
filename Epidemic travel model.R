f41ba00036397f79dc09d11613935dbf6150a630
Nishant Kishore <nish.kishore@gmail.com>
2020-04-28 16:16:47 -0400

    Update Epidemic travel model.R

--- Epidemic travel model.R
+++ Epidemic travel model.R
@@ -2,11 +2,11 @@
 source("./src/dependencies.R")
 source("./src/models.R")
 source("./src/helper_functions.R")
-source("./src/generate_plots.R")
+#source("./src/generate_plots.R")
 
 
-#run model using inputs from driver file 
+#run model using inputs from driver file
 run_models("./drivers/driver_v2.yaml", cores = 10)
 
-#generate heatmaps 
-generate_plots(run_all = T)
+#generate heatmaps
+#generate_plots(run_all = T)
