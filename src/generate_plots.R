86642cddb7b19b28847aad8f1227735f7496891d
Rebecca Kahn <rek160@mail.harvard.edu>
2020-06-15 23:36:13 -0400

    add new comparison heatmap

--- src/generate_plots.R
+++ src/generate_plots.R
@@ -121,36 +121,228 @@ summary_stats <- function(row){
   
 }
 
-rows <- seq(17,400,1)
+# rows <- seq(17,400,1)
+# 
+# lapply(rows,function(i)summary_stats(i)) %>%
+#   bind_rows() %>%
+#   mutate(alpha_beta = paste0("travel inc: ",alpha_inc,
+#                              "; beta inc: ", beta_inc,
+#                              "; travel dec: ",alpha_dec,
+#                              "; beta dec: ",beta_dec),
+#          param_sums = paste0("initial: ",  num_inf,
+#                              "; travel: ", alpha_init,
+#                              "; trigger: ",cases_ld_a,
+#                              "; delay: ",ld_b))-> summary_results
+# 
+# ggplot(summary_results,aes(x=param_sums,y=alpha_beta)) +
+#   geom_tile(aes(fill=av_prob_epi),size=2,width=0.8,height=0.8) +
+#   scale_color_grey(start = 1, end = 0)+
+#   #geom_text(aes(label = round(av_start_time)), fontface = "bold",color="black") +
+#   geom_text(aes(label = round(av_prob_epi,1)), fontface = "bold",color="black") +
+#   scale_fill_viridis_c(limits=c(0,1)) + 
+#   theme_classic() +
+#   theme(legend.position = "bottom", axis.ticks = element_blank(),
+#         axis.text.x=element_text(angle=90),
+#         #axis.text.x = element_blank(),
+#         #axis.text.y = element_blank(),   
+#         axis.title.x = element_blank(),
+#         axis.title.y = element_blank(),
+#         axis.line = element_blank()) + scale_alpha(guide = 'none')+
+#   labs(fill="Probability have 10+ cases",
+#        #x="Other parameters",
+#        #y="Travel & beta",
+#        color=element_blank(),
+#        alpha=element_blank())
 
-lapply(rows,function(i)summary_stats(i)) %>%
-  bind_rows() %>%
-  mutate(alpha_beta = paste0("travel inc: ",alpha_inc,
-                             "; beta inc: ", beta_inc,
-                             "; travel dec: ",alpha_dec,
-                             "; beta dec: ",beta_dec),
-         param_sums = paste0("initial: ",  num_inf,
-                             "; travel: ", alpha_init,
-                             "; trigger: ",cases_ld_a,
-                             "; delay: ",ld_b))-> summary_results
 
-ggplot(summary_results,aes(x=param_sums,y=alpha_beta)) +
-  geom_tile(aes(fill=av_prob_epi),size=2,width=0.8,height=0.8) +
-  scale_color_grey(start = 1, end = 0)+
-  #geom_text(aes(label = round(av_start_time)), fontface = "bold",color="black") +
-  geom_text(aes(label = round(av_prob_epi,1)), fontface = "bold",color="black") +
-  scale_fill_viridis_c(limits=c(0,1)) + 
-  theme_classic() +
-  theme(legend.position = "bottom", axis.ticks = element_blank(),
-        axis.text.x=element_text(angle=90),
-        #axis.text.x = element_blank(),
-        #axis.text.y = element_blank(),   
-        axis.title.x = element_blank(),
-        axis.title.y = element_blank(),
-        axis.line = element_blank()) + scale_alpha(guide = 'none')+
-  labs(fill="Probability have 10+ cases",
-       #x="Other parameters",
-       #y="Travel & beta",
-       color=element_blank(),
-       alpha=element_blank())
+
+create_heatmap3 <- function(rds_id1,rds_id2,comm_version,threshold,day){
+  
+  suppressMessages(
+    params1 <- read_csv("./results_log.csv") %>% subset(unique_id == rds_id1)
+  )
+  
+  data1 <- read_rds(paste0("./cache/results/",rds_id1,".rds"))
+  
+  suppressMessages(
+    params2 <- read_csv("./results_log.csv") %>% subset(unique_id == rds_id2)
+  )
+  
+  data2 <- read_rds(paste0("./cache/results/",rds_id2,".rds"))
+  
+  
+  comm_type_list <- get_comm_types(params1$num_communities,comm_version)
+  
+  city <- comm_type_list[[1]]
+  noncity <- c(comm_type_list[[2]],comm_type_list[[3]])
+  
+  data1$type <- ifelse(data1$Community %in% city, "City","Non-city")
+  data2$type <- ifelse(data2$Community %in% city, "City","Non-city")
+  
+  num_edge <- sqrt(params1$num_communities)
+  row <- c(rep(1:num_edge,each=10))
+  col <- c(rep(1:num_edge,times=10))
+  
+  # summarise total # infections and start times by community
+  
+  data1 %>%
+    group_by(Simulation,DayInfected, Community, type, t_ld_a) %>%
+    summarise(n=n()) %>%
+    ungroup() %>%
+    group_by(Community) %>%
+    mutate(cumulative=cumsum(n)) %>%
+    ungroup() -> data1
+  
+  data1 %>%
+    filter(DayInfected<=day) %>%
+    group_by(Community,Simulation, type) %>%
+    summarise(infections = sum(n),
+              start = min(DayInfected),
+              t_ld_a = min(t_ld_a)) %>%
+    ungroup() %>%
+    group_by(Community, type) %>%
+    summarise(prob_epi = round(sum(infections>=threshold)/params1$Nruns,2),
+              av_start_time = mean(start)) %>%
+    mutate(row = row[Community],
+           col = col[Community]) -> summary_stats1
+  
+  excluded <- setdiff(1:100,summary_stats1$Community)
+  type_excluded <- ifelse(excluded %in% city, "City","Non-city")
+  
+  bind_cols("Community"=excluded,
+            "type"=type_excluded,
+            "prob_epi"=rep(0,length(excluded)),
+            "av_start_time"=rep(NA,length(excluded)),
+            "row"=row[excluded],
+            "col"=col[excluded]) %>%
+    as_tibble() %>%
+    bind_rows(summary_stats1)-> summary_stats1_complete
+  
+  data2 %>%
+    group_by(Simulation,DayInfected, Community, type, t_ld_a) %>%
+    summarise(n=n()) %>%
+    ungroup() %>%
+    group_by(Community) %>%
+    mutate(cumulative=cumsum(n)) %>%
+    ungroup() -> data2
+  
+  data2 %>%
+    filter(DayInfected<=day) %>%
+    group_by(Community,Simulation, type) %>%
+    summarise(infections = sum(n),
+              start = min(DayInfected),
+              t_ld_a = min(t_ld_a)) %>%
+    ungroup() %>%
+    group_by(Community, type) %>%
+    summarise(prob_epi = round(sum(infections>=threshold)/params1$Nruns,2),
+              av_start_time = mean(start)) %>%
+    mutate(row = row[Community],
+           col = col[Community]) -> summary_stats2
+  
+  excluded <- setdiff(1:100,summary_stats2$Community)
+  type_excluded <- ifelse(excluded %in% city, "City","Non-city")
+  
+  bind_cols("Community"=excluded,
+            "type"=type_excluded,
+            "prob_epi"=rep(0,length(excluded)),
+            "av_start_time"=rep(NA,length(excluded)),
+            "row"=row[excluded],
+            "col"=col[excluded]) %>%
+    as_tibble() %>%
+    bind_rows(summary_stats2)-> summary_stats2_complete
+  
+  summary_stats2_complete %>%
+    right_join(summary_stats1_complete, by="Community") %>%
+    mutate(av_start_time.x=case_when(is.na(av_start_time.x)~0, TRUE~av_start_time.x),
+           av_start_time.y=case_when(is.na(av_start_time.y)~0, TRUE~av_start_time.y),
+           perc_change_label = round((prob_epi.x - prob_epi.y)/prob_epi.y*100,1),
+           perc_change_time_label = round((av_start_time.x - av_start_time.y)/av_start_time.y*100,1)
+    )-> summary_stats
+  
+  summary_stats$perc_change <- ifelse(is.infinite(summary_stats$perc_change_label), 
+                                      max(summary_stats$perc_change_label[is.finite(summary_stats$perc_change_label)])*1.5,
+                                      summary_stats$perc_change_label)
+  
+  summary_stats$perc_change_time <- ifelse(is.infinite(summary_stats$perc_change_time_label), 
+                                           min(summary_stats$perc_change_time_label[is.finite(summary_stats$perc_change_time_label)])*1.5,
+                                           summary_stats$perc_change_time_label)
+  
+  heatmap1 <- ggplot(summary_stats,aes(x=col.x,y=row.x)) +
+    geom_tile(aes(fill=perc_change,color=type.x),size=2,width=0.8,height=0.8) +
+    #scale_color_brewer(type = "qual", palette = "Dark2") +
+    scale_color_grey(start = 1, end = 0)+
+    geom_text(aes(label = perc_change_label), fontface = "bold",color="black") +
+    #scale_fill_gradient(high = "red", low = muted("green")) +
+    #scale_fill_viridis_c(limits=c(-1,1)) + 
+    scale_fill_gradient2(
+      low = muted("blue"),
+      mid = "white",
+      high = muted("red"),
+      midpoint = 0,
+      space = "Lab",
+      na.value = "grey",
+      guide = "colourbar",
+      aesthetics = "fill"
+    )+
+    theme_classic() +
+    theme(legend.position = "bottom", axis.ticks = element_blank(),
+          axis.title.x = element_blank(),axis.title.y = element_blank(),
+          axis.text.x = element_blank(), axis.text.y = element_blank(),
+          axis.line = element_blank()) + scale_alpha(guide = 'none')+
+    labs(fill=paste0("% change in probability of ",threshold, " cases by day ", day),
+         color=element_blank(),
+         alpha=element_blank(),
+         caption=paste0("Comparing relative travel after restrictions announced: ", params2$alpha_inc, " to ", params1$alpha_inc,
+                        "\nCases to trigger restrictions = ", params1$cases_ld_a,
+                        "\nDays between restrictions annnounced and begin = ",params1$ld_b, 
+                        "\nRelative beta after restrictions announced = ", params1$beta_inc, "; Relative beta after restrictions in place = ", params1$beta_dec, 
+                        "\nInitial movement prob = ",params1$alpha_init, "; Relative travel after restrictions in place = ", params1$alpha_dec))
+  
+  heatmap2 <- ggplot(summary_stats,aes(x=col.x,y=row.x)) +
+    geom_tile(aes(fill=perc_change_time,color=type.x),size=2,width=0.8,height=0.8) +
+    #scale_color_brewer(type = "qual", palette = "Dark2") +
+    scale_color_grey(start = 1, end = 0)+
+    geom_text(aes(label = perc_change_time_label), fontface = "bold",color="black") +
+    #scale_fill_gradient(high = "red", low = muted("green")) +
+    #scale_fill_viridis_c(limits=c(-1,1)) + 
+    scale_fill_gradient2(
+      low = muted("red"),
+      mid = "white",
+      high = muted("blue"),
+      midpoint = 0,
+      space = "Lab",
+      na.value = "grey",
+      guide = "colourbar",
+      aesthetics = "fill"
+    )+
+    theme_classic() +
+    theme(legend.position = "bottom", axis.ticks = element_blank(),
+          axis.title.x = element_blank(),axis.title.y = element_blank(),
+          axis.text.x = element_blank(), axis.text.y = element_blank(),
+          axis.line = element_blank()) + scale_alpha(guide = 'none')+
+    labs(fill=paste0("% change in start time "),
+         color=element_blank(),
+         alpha=element_blank(),
+         caption=paste0("Comparing relative travel after restrictions announced: ", params2$alpha_inc, " to ", params1$alpha_inc,
+                        "\nCases to trigger restrictions = ", params1$cases_ld_a,
+                        "\nDays between restrictions annnounced and begin = ",params1$ld_b, 
+                        "\nRelative beta after restrictions announced = ", params1$beta_inc, "; Relative beta after restrictions in place = ", params1$beta_dec, 
+                        "\nInitial movement prob = ",params1$alpha_init, "; Relative travel after restrictions in place = ", params1$alpha_dec))
+  
+  
+  list(heatmap1,heatmap2)
+  
+}
+
+require(googledrive)
+
+
+rds_id1 <- "e7dcae7fec4ccad9dda1e1484a2bbb1b"
+rds_id2 <- "6e489021a5ea5508cbf920a6a6b45511"
+
+create_heatmap3(rds_id1,rds_id2,3,1,30)[[1]]
+create_heatmap3(rds_id1,rds_id2,3,1,30)[[2]]
+
+
+
 
