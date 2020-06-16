49906b0fa568381fcdc39c888a089abafa13ccfd
Rebecca Kahn <rek160@mail.harvard.edu>
2020-06-16 15:35:33 -0400

    code for megaplots

--- src/generate_plots.R
+++ src/generate_plots.R
@@ -2,7 +2,7 @@
 source("./src/helper_functions.R")
 library(scales)
 
-create_heatmap <- function(rds_id){
+create_heatmap <- function(rds_id,comm_version,threshold,day){
   
   suppressMessages(
     params <- read_csv("./results_log.csv") %>% subset(unique_id == rds_id)
@@ -10,64 +10,121 @@ create_heatmap <- function(rds_id){
   
   data <- read_rds(paste0("./cache/results/",rds_id,".rds"))
   
-  comm_type_list <- get_comm_types(params$num_communities)
+  data %>%
+    group_by(Simulation,DayInfected, Community, type, t_ld_a) %>%
+    summarise(n=n()) %>%
+    ungroup() %>%
+    group_by(Community) %>%
+    mutate(cumulative=cumsum(n)) %>%
+    ungroup() -> data1
   
-  urban <- comm_type_list[[1]]
-  suburban <- comm_type_list[[2]]
-  rural <- comm_type_list[[3]]
+  comm_type_list <- get_comm_types(params1$num_communities,comm_version)
   
-  num_edge <- sqrt(params$num_communities)
+  city <- comm_type_list[[1]]
+  noncity <- c(comm_type_list[[2]],comm_type_list[[3]])
+  
+  data1$type <- ifelse(data1$Community %in% city, "City","Non-city")
+
+  num_edge <- sqrt(params1$num_communities)
   row <- c(rep(1:num_edge,each=10))
   col <- c(rep(1:num_edge,times=10))
   
   # summarise total # infections and start times by community
-  data %>%
+  
+  data1 %>%
+    group_by(Simulation,DayInfected, Community, type, t_ld_a) %>%
+    summarise(n=n()) %>%
+    ungroup() %>%
+    group_by(Community) %>%
+    mutate(cumulative=cumsum(n)) %>%
+    ungroup() -> data1
+  
+  data1 %>%
+    filter(DayInfected<=day) %>%
     group_by(Community,Simulation, type) %>%
     summarise(infections = sum(n),
               start = min(DayInfected),
               t_ld_a = min(t_ld_a)) %>%
     ungroup() %>%
     group_by(Community, type) %>%
-    summarise(prob_epi = round(sum(infections>=params$cases_ld_a)/params$Nruns,2),
-              av_start_time = round(mean(start - t_ld_a),1)) %>%
+    summarise(prob_epi = round(sum(infections>=threshold)/params1$Nruns,2),
+              av_start_time = mean(start)) %>%
     mutate(row = row[Community],
-           col = col[Community]) -> summary_stats
+           col = col[Community]) -> summary_stats1
   
-  excluded <- setdiff(1:100,summary_stats$Community)
-  type_excluded <- ifelse(excluded %in% urban, "Urban",
-                          ifelse(excluded %in% suburban, "Suburban", "Rural"))
+  excluded <- setdiff(1:100,summary_stats1$Community)
+  type_excluded <- ifelse(excluded %in% city, "City","Non-city")
   
   bind_cols("Community"=excluded,
-            "type"=type_excluded,
+            "type"=type_excluded %>% as.character(),
             "prob_epi"=rep(0,length(excluded)),
             "av_start_time"=rep(NA,length(excluded)),
             "row"=row[excluded],
             "col"=col[excluded]) %>%
     as_tibble() %>%
-    bind_rows(summary_stats)-> summary_stats_complete
+    bind_rows(summary_stats1)-> summary_stats1_complete
   
-  heatmap <- ggplot(summary_stats_complete,aes(x=col,y=row)) +
-    geom_tile(aes(fill=prob_epi,color=type,alpha=0.5),size=2,width=0.8,height=0.8) +
+  
+  
+  heatmap1 <- ggplot(summary_stats1_complete,aes(x=col,y=row)) +
+    geom_tile(aes(fill=prob_epi,color=type),size=2,width=0.8,height=0.8) +
     #scale_color_brewer(type = "qual", palette = "Dark2") +
     scale_color_grey(start = 1, end = 0)+
-    geom_text(aes(label = av_start_time), fontface = "bold",color="black") +
+    geom_text(aes(label = prob_epi), fontface = "bold",color="white") +
+    #scale_fill_viridis_c(limits=c(0,1)) + 
+    theme_classic() +
+    theme(legend.position = "bottom", axis.ticks = element_blank(),
+          axis.title.x = element_blank(),axis.title.y = element_blank(),
+          axis.text.x = element_blank(), axis.text.y = element_blank(),
+          axis.line = element_blank()) + scale_alpha(guide = 'none') +
+    labs(fill=paste0("probability of ",threshold, " cases by day ", day),
+         color=element_blank(),
+         alpha=element_blank())
+    # labs(fill=paste0("% change in probability of ",threshold, " cases by day ", day),
+    #      color=element_blank(),
+    #      alpha=element_blank(),
+    #      caption=paste0("Comparing relative travel after restrictions announced: ", params2$alpha_inc, " to ", params1$alpha_inc,
+    #                     "\nCases to trigger restrictions = ", params1$cases_ld_a,
+    #                     "\nDays between restrictions annnounced and begin = ",params1$ld_b, 
+    #                     "\nRelative beta after restrictions announced = ", params1$beta_inc, "; Relative beta after restrictions in place = ", params1$beta_dec, 
+    #                     "\nInitial movement prob = ",params1$alpha_init, "; Relative travel after restrictions in place = ", params1$alpha_dec))
+  
+  heatmap2 <- ggplot(summary_stats1_complete,aes(x=col,y=row)) +
+    geom_tile(aes(fill=av_start_time,color=type),size=2,width=0.8,height=0.8) +
+    #scale_color_brewer(type = "qual", palette = "Dark2") +
+    scale_color_grey(start = 1, end = 0)+
+    geom_text(aes(label = round(av_start_time)), fontface = "bold",color="white") +
     #scale_fill_gradient(high = "red", low = muted("green")) +
-    scale_fill_viridis_c(limits=c(0,1)) + 
+    #scale_fill_viridis_c(limits=c(0,1)) + 
+    # scale_fill_gradient2(
+    #   low = muted("red"),
+    #   mid = "white",
+    #   high = muted("blue"),
+    #   midpoint = 0,
+    #   space = "Lab",
+    #   na.value = "grey",
+    #   guide = "colourbar",
+    #   aesthetics = "fill"
+    # )+
     theme_classic() +
     theme(legend.position = "bottom", axis.ticks = element_blank(),
           axis.title.x = element_blank(),axis.title.y = element_blank(),
           axis.text.x = element_blank(), axis.text.y = element_blank(),
           axis.line = element_blank()) + scale_alpha(guide = 'none')+
-    labs(fill="Probability epidemic",
+    labs(fill="Average start time",
          color=element_blank(),
-         alpha=element_blank(),
-         caption=paste0("Number in cell denotes average time of first case relative to announcement of restrictions",
-                       "\nCases to trigger restrictions = ", params$cases_ld_a, "; Days between restrictions annnounced and begin = ",params$ld_b, 
-                       "\nRelative beta after restrictions announced = ", params$beta_inc, "; Relative beta after restrictions in place = ", params$beta_dec, 
-                       "\nInitial movement prob = ",params$alpha_init,"; Relative travel after restrictions announced = ", params$alpha_inc,"; Relative travel after restrictions in place = ", params$alpha_dec))
-  
-  
-  return(heatmap)
+         alpha=element_blank())
+    # labs(fill=paste0("% change in start time "),
+    #      color=element_blank(),
+    #      alpha=element_blank(),
+    #      caption=paste0("Comparing relative travel after restrictions announced: ", params2$alpha_inc, " to ", params1$alpha_inc,
+    #                     "\nCases to trigger restrictions = ", params1$cases_ld_a,
+    #                     "\nDays between restrictions annnounced and begin = ",params1$ld_b, 
+    #                     "\nRelative beta after restrictions announced = ", params1$beta_inc, "; Relative beta after restrictions in place = ", params1$beta_dec, 
+    #                     "\nInitial movement prob = ",params1$alpha_init, "; Relative travel after restrictions in place = ", params1$alpha_dec))
+    # 
+    # 
+  list(heatmap1,heatmap2)
   
 }
 
@@ -335,11 +392,7 @@ create_heatmap3 <- function(rds_id1,rds_id2,comm_version,threshold,day){
   
 }
 
-require(googledrive)
-
 results_log <- read_csv("./results_log.csv")
-
-# high alpha_init, 10 trigger cases
 
 results_log %>%
   subset(alpha_init == 0.01 & cases_ld_a==10 & alpha_dec == 0.5 & beta_dec == 0.5) -> results
@@ -360,12 +413,75 @@ rds_id_alpha2.0_beta2.0 <- results %>% subset(alpha_inc==2 & beta_inc==2) %>% pu
 rds_id_alpha2.5_beta2.0 <- results %>% subset(alpha_inc==2.5 & beta_inc==2) %>% pull(unique_id) 
 rds_id_alpha3.0_beta2.0 <- results %>% subset(alpha_inc==3 & beta_inc==2) %>% pull(unique_id) 
 
-baseline <- create_heatmap2()
+baseline <- create_heatmap(rds_id_alpha1.0_beta1.0,3,1,30)
+baseline_prob <- baseline[[1]]
+baseline_time <- baseline[[2]]
 
-output <- create_heatmap3(rds_id1,rds_id2,3,1,30)
+output <- create_heatmap3(rds_id_alpha1.0_beta1.0,rds_id_alpha1.5_beta1.0,3,1,30)
 alpha1.5_beta1.0_prob <- output[[1]]
 alpha1.5_beta1.0_time <- output[[2]]
 
+output <- create_heatmap3(rds_id_alpha1.0_beta1.0,rds_id_alpha2.0_beta1.0,3,1,30)
+alpha2.0_beta1.0_prob <- output[[1]]
+alpha2.0_beta1.0_time <- output[[2]]
+
+output <- create_heatmap3(rds_id_alpha1.0_beta1.0,rds_id_alpha2.5_beta1.0,3,1,30)
+alpha2.5_beta1.0_prob <- output[[1]]
+alpha2.5_beta1.0_time <- output[[2]]
+
+output <- create_heatmap3(rds_id_alpha1.0_beta1.0,rds_id_alpha3.0_beta1.0,3,1,30)
+alpha3.0_beta1.0_prob <- output[[1]]
+alpha3.0_beta1.0_time <- output[[2]]
+
+output <- create_heatmap3(rds_id_alpha1.0_beta1.0,rds_id_alpha1.0_beta1.5,3,1,30)
+alpha1.0_beta1.5_prob <- output[[1]]
+alpha1.0_beta1.5_time <- output[[2]]
+
+output <- create_heatmap3(rds_id_alpha1.0_beta1.0,rds_id_alpha1.5_beta1.5,3,1,30)
+alpha1.5_beta1.5_prob <- output[[1]]
+alpha1.5_beta1.5_time <- output[[2]]
+
+output <- create_heatmap3(rds_id_alpha1.0_beta1.0,rds_id_alpha2.0_beta1.5,3,1,30)
+alpha2.0_beta1.5_prob <- output[[1]]
+alpha2.0_beta1.5_time <- output[[2]]
+
+output <- create_heatmap3(rds_id_alpha1.0_beta1.0,rds_id_alpha2.5_beta1.5,3,1,30)
+alpha2.5_beta1.5_prob <- output[[1]]
+alpha2.5_beta1.5_time <- output[[2]]
+
+output <- create_heatmap3(rds_id_alpha1.0_beta1.0,rds_id_alpha3.0_beta1.5,3,1,30)
+alpha3.0_beta1.5_prob <- output[[1]]
+alpha3.0_beta1.5_time <- output[[2]]
+
+output <- create_heatmap3(rds_id_alpha1.0_beta1.0,rds_id_alpha1.0_beta2.0,3,1,30)
+alpha1.0_beta2.0_prob <- output[[1]]
+alpha1.0_beta2.0_time <- output[[2]]
+
+output <- create_heatmap3(rds_id_alpha1.0_beta1.0,rds_id_alpha1.5_beta2.0,3,1,30)
+alpha1.5_beta2.0_prob <- output[[1]]
+alpha1.5_beta2.0_time <- output[[2]]
+
+output <- create_heatmap3(rds_id_alpha1.0_beta1.0,rds_id_alpha2.0_beta2.0,3,1,30)
+alpha2.0_beta2.0_prob <- output[[1]]
+alpha2.0_beta2.0_time <- output[[2]]
+
+output <- create_heatmap3(rds_id_alpha1.0_beta1.0,rds_id_alpha2.5_beta2.0,3,1,30)
+alpha2.5_beta2.0_prob <- output[[1]]
+alpha2.5_beta2.0_time <- output[[2]]
+
+output <- create_heatmap3(rds_id_alpha1.0_beta1.0,rds_id_alpha3.0_beta2.0,3,1,30)
+alpha3.0_beta2.0_prob <- output[[1]]
+alpha3.0_beta2.0_time <- output[[2]]
+
+panel_prob <- ggarrange(baseline_prob,alpha1.5_beta1.0_prob,alpha2.0_beta1.0_prob,alpha2.5_beta1.0_prob,alpha3.0_beta1.0_prob,
+                   alpha1.0_beta1.5_prob,alpha1.5_beta1.5_prob,alpha2.0_beta1.5_prob,alpha2.5_beta1.5_prob,alpha3.0_beta1.5_prob,
+                   alpha1.0_beta2.0_prob,alpha1.5_beta2.0_prob,alpha2.0_beta2.0_prob,alpha2.5_beta2.0_prob,alpha3.0_beta2.0_prob,
+                   nrow=5)
+
+panel_time <- ggarrange(baseline_time,alpha1.5_beta1.0_time,alpha2.0_beta1.0_time,alpha2.5_beta1.0_time,alpha3.0_beta1.0_time,
+                        alpha1.0_beta1.5_time,alpha1.5_beta1.5_time,alpha2.0_beta1.5_time,alpha2.5_beta1.5_time,alpha3.0_beta1.5_time,
+                        alpha1.0_beta2.0_time,alpha1.5_beta2.0_time,alpha2.0_beta2.0_time,alpha2.5_beta2.0_time,alpha3.0_beta2.0_time,
+                        nrow=5)
 
 
 
