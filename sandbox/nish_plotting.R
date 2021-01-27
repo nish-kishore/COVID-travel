db9a9e866bcda752e72f38339f14683b058f2f1b
Nishant Kishore <nish.kishore@gmail.com>
2021-01-27 11:13:20 -0600

    grav model spain test

--- sandbox/nish_plotting.R
+++ sandbox/nish_plotting.R
@@ -1,4 +1,5 @@
 source("./src/helper_functions.R")
+library(sf)
 
 travel_probs <- read_rds("./testing/travel_probs.rds")
 results_log <- read_csv("./results_log.csv")
@@ -156,55 +157,125 @@ ggsave("./figs/prop_comm_import.png", dpi = 300)
 
 #Spain results
 ids <- subset(results_log, comm_version == 5 & 
-                mob_net_name %in% c("mob_net_spain", "mob_net_control") & 
+                mob_net_name %in% c("mob_net_spain") & 
                 beta_dec == 0.5 & 
                 alpha_dec == 0.5) %>% dplyr::select(unique_id)
 
 lapply(1:nrow(ids), function(x) load_merge_vars(results_log, ids[x,], beta_inc, alpha_inc, beta_dec, alpha_dec, mob_net_name)) %>%
-  bind_rows() -> plot_data
+  bind_rows() -> int_data
 
-plot_data %>%
-  dplyr::select(Simulation, Community, DayInfected, Symptoms, beta_inc, alpha_inc, mob_net_name) %>%
-  group_by(Simulation, Community, DayInfected, beta_inc, alpha_inc, mob_net_name) %>%
-  summarize(tot_inf = sum(Symptoms)) %>%
-  ungroup() %>%
-  group_by(Community, DayInfected, beta_inc, alpha_inc, mob_net_name) %>%
-  summarise(tot_inf = mean(tot_inf)) %>%
+ids <- subset(results_log, comm_version == 5 & 
+                mob_net_name %in% c("mob_net_control") & 
+                beta_dec == 0.5 & 
+                alpha_dec == 0.5) %>% dplyr::select(unique_id)
+
+lapply(1:nrow(ids), function(x) load_merge_vars(results_log, ids[x,], beta_inc, alpha_inc, beta_dec, alpha_dec, mob_net_name)) %>%
+  bind_rows() -> cont_data
+
+bind_rows(int_data, cont_data) %>%
+  dplyr::select(Simulation, DayInfected, beta_inc, alpha_inc, mob_net_name) %>%
+  group_by(Simulation, DayInfected, beta_inc, alpha_inc, mob_net_name) %>%
+  summarize(tot_inf = n()) %>%
   ungroup() %>%
   group_by(DayInfected, beta_inc, alpha_inc, mob_net_name) %>%
-  summarise(tot_inf = sum(tot_inf)) -> x
+  summarise(tot_inf = mean(tot_inf)) %>%
+  ungroup() -> x
 
 x %>%
-  mutate(type = ifelse(mob_net_name == "mob_net_control", "Control", "Intervention")) %>%
+  mutate(type = ifelse(mob_net_name == "mob_net_control", "Normally Dispersed", "Clustered due to lockdown")) %>%
+  group_by(beta_inc, alpha_inc, type) %>%
+  arrange(DayInfected) %>%
+  mutate(cum_inf = cumsum(tot_inf)) %>%
   ggplot(aes(x = DayInfected, y = tot_inf, group = type, color = type)) + 
   geom_line() + 
-  facet_grid(beta_inc ~ alpha_inc)
+  facet_grid(beta_inc ~ alpha_inc) + 
+  labs(x = "Days", y = "# of Infections", group = "Type", color = "Type") + 
+  theme_bw()
 
+#Percent change in probability of having at least 1 case by 30 days (left); 
+ref <- tibble("Community" = 1:100, 
+       "row" = sapply(1:10, function(x) rep(x,10),simplify = F) %>% unlist(),
+       "column" = rep(1:10,10))
 
+ref <- read_rds("./cache/new_loc_names.rds") %>% sort() %>%
+  as_tibble() %>%
+  mutate(Community = 1:100) %>%
+  full_join(ref, by = "Community") %>%
+  rename("comm_name" = "value") 
 
+bind_rows(int_data, cont_data) %>%
+  #subset(beta_inc == 1 & alpha_inc == 1 & mob_net_name == "mob_net_spain") %>%
+  dplyr::select(Community, Simulation, DayInfected, beta_inc, alpha_inc, mob_net_name) %>%
+  group_by(Community, Simulation, DayInfected, beta_inc, alpha_inc, mob_net_name) %>%
+  summarise(inf = n()) %>%
+  ungroup() -> y
 
-
-  group_by(Community, alpha_inc, beta_dec) %>%
-  arrange(cumulative) %>%
-  subset(cumulative >= day_till) %>%
-  group_by(Community,Simulation, sim_type) %>%
+y %>%
+  subset(DayInfected <= 30) %>%
+  group_by(Simulation, Community, beta_inc, alpha_inc, mob_net_name) %>%
   slice(1) %>%
   ungroup() %>%
-  group_by(Community) %>%
-  mutate(nsims=length(n)) %>%
+  group_by(Community, beta_inc, alpha_inc, mob_net_name) %>%
+  summarise(count = n()/100) %>%
   ungroup() %>%
-  group_by(Community, sim_type) %>%
-  summarise(avg_day_to_n = sum(DayInfected)/mean(nsims)) %>%
-  ungroup() -> out_data
+  complete(Community, beta_inc, alpha_inc, mob_net_name, fill = list(count = 0)) -> test
+ 
+spain_map <- st_read("G:/My Drive/HSPH/CCDD/travel_surge/data/gis/Spain/ESP_adm2.shp")
+library(mapSpain)
 
-subset(out_data, sim_type == "Control") %>%
-  dplyr::select(Community, avg_day_to_n) %>%
-  rename("ctrl_day_to_n" = "avg_day_to_n") %>%
-  right_join(out_data, by = "Community") %>%
-  ggplot(aes(x = ctrl_day_to_n, y = avg_day_to_n, color = sim_type)) +
-  geom_point() + 
-  geom_smooth(method = "loess", se = F) + 
+spain_hex_prov <- esp_get_hex_prov()
+spain_hex_prov <- esp_get_grid_prov()
+
+test %>%
+  subset(alpha_inc == 1.5 & beta_inc == 1.5) %>%
+  mutate(type = ifelse(mob_net_name == "mob_net_control", "Normal mobility network", "Clustered mobility network due to lockdown")) %>%
+  left_join(ref, by = "Community") %>%
+  mutate(comm_name = case_when(
+    comm_name == "Madrid_RTQZ" ~ "Madrid", 
+    comm_name == "Madrid" ~ "Otro", 
+    T ~ comm_name
+  )) %>%
+  {merge(spain_map, ., by.x = "NAME_2", by.y = "comm_name")} %>% 
+  ggplot() + 
+  geom_sf(aes(fill = count)) + 
+  geom_sf(data = subset(spain_map, NAME_2 == "Madrid"), size = 2, fill = NA, color = "black") +
+  scale_fill_gradient2(low = muted("green"), high = muted("red")) + 
+  facet_wrap(~type) +
+  labs(fill = "Probability of 1 case within 30 days") + 
   theme_bw() + 
-  labs(x = "Control - Avg days till X cases", y = "Comparison - Avg days till X cases",
-       title = paste0("Avg # of days till ",day_till, " cases / Lockdown after: ", cases_ld, " cases"), 
-       color = "Sim Type")
+  theme(legend.position = "bottom")
+
+test %>%
+  subset(alpha_inc == 1.5 & beta_inc == 1.5) %>%
+  mutate(type = ifelse(mob_net_name == "mob_net_control", "Normal mobility network", "Clustered mobility network due to lockdown")) %>%
+  left_join(ref, by = "Community") %>%
+  mutate(comm_name = case_when(
+    comm_name == "Madrid_RTQZ" ~ "Madrid", 
+    comm_name == "Madrid" ~ "Otro", 
+    T ~ comm_name
+  )) %>%
+  {merge(spain_hex_prov, ., by.x = "prov.shortname.es", by.y = "comm_name")} %>% 
+  ggplot() + 
+  geom_sf(data = spain_hex_prov) +
+  geom_sf(aes(fill = count)) + 
+  geom_sf(data = subset(spain_hex_prov, prov.shortname.es == "Madrid"), size = 2, fill = NA, color = "black") +
+  geom_sf_text(data = spain_hex_prov, aes(label = label)) +
+  scale_fill_gradient2(high = muted("red")) + 
+  facet_wrap(~type) +
+  labs(fill = "Probability of 1 case within 30 days") + 
+  theme_bw() + 
+  theme(legend.position = "bottom", 
+        axis.title = element_blank())
+
+ggsave("./figs/spain_hex_map.png")
+
+subset(cases_ld_a %in% c(9999, cases_ld) & beta_inc == 1.5 & beta_dec == 0.5 & alpha_dec == 0.5) %>% 
+  arrange(cumulative) %>%
+  subset(cumulative >= day_till & DayInfected<=day) %>%
+  group_by(Community,sim_type) %>%
+  slice(1) %>%
+  ungroup() %>%
+  group_by(sim_type) %>%
+  summarise(ncomm=length(Community)) -> out_data
+
+#Percent change in the number of days tillthe first case (right)
